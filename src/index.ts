c65c472935051874bac9308b265c3aa20dbaa431
Fábio Priamo <fhpriamo@users.noreply.github.com>
2021-03-25 18:28:11 -0300

    chore: remove sample files

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { greet } from './greetings';
-import { userInfo } from 'os';
-
-const hours = new Date().getHours();
-
-const username: string =
-  process.argv[2] || userInfo({ encoding: 'utf8' }).username;
-
-console.log(greet((daytime) => `Good ${daytime}, ${username}!`, hours));
